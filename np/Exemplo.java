e0e986a798c3e21e0198bcc645a3ff1ceb8fe92a
Johnpedrox <jpedrox01@outlook.com>
2017-03-29 11:49:13 -0300

    SDS

---
+++ np/Exemplo.java
@@ -0,0 +1,12 @@
+	public class Exemplo{
+		public static int x = 2;
+		int y = 5;
+		public void incrementar(){
+			Exemplo.x++;
+		}
+		public static void dec(){
+			Exemplo.x--;
+			//y = 6 <- Erro
+		}
+	}
+	
